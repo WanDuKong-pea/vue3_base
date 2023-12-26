445dc3125f48acd73336c807255b8544ba80f271
KimYeJin <sophisticateo_o@naver.com>
2023-12-26 17:31:59 +0900

    이벤트 처리

---
+++ src/lecture/App_event.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<button @click="printEventInfo('Hello Vue3', $event)">
+			inline event handelr
+		</button>
+		<hr />
+		<input type="text" @keyup="onKeyupHandler($event)" />
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		const printEventInfo = (message, event) => {
+			console.log('message: ', message);
+			console.log('event: ', event);
+			console.log('event.target: ', event.target);
+			console.log('event.target.tagName: ', event.target.tagName);
+		};
+
+		const onKeyupHandler = event => {
+			console.log('event.key: ', event.key);
+			console.log('event.target.value: ', event.target.value);
+		};
+		return {
+			printEventInfo,
+			onKeyupHandler,
+		};
+	},
+};
+</script>
+
+<style></style>
---
+++ src/lecture/App_mouse.vue
@@ -0,0 +1,70 @@
+<template>
+	<div>
+		<div id="modifiers">
+			<div @click.capture="clickDiv">
+				DIV
+				<p @click.capture="clickP">
+					P
+					<!-- event.stopPropagation === @click.stop -->
+					<!-- <span @click.stop="clickSpan"> SPAN </span> -->
+					<span @click="clickSpan"> SPAN </span>
+					<span @click.once="clickSpan"> SPAN </span>
+				</p>
+			</div>
+		</div>
+		<!-- event.preventDefault() === @click.prevent -->
+		<a href="https://www.naver.com" @click.prevent="clickA">NAVER</a>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		const clickDiv = () => {
+			console.log('clickDiv');
+			//location.href = 'https://www.naver.com';
+		};
+		const clickP = () => {
+			console.log('clickP');
+		};
+		const clickSpan = () => {
+			//event.stopPropagation();
+			console.log('clickSpan');
+			//alert('좋아요');
+		};
+		const clickA = () => {
+			alert('clickA');
+		};
+
+		return {
+			clickDiv,
+			clickP,
+			clickSpan,
+			clickA,
+		};
+	},
+};
+</script>
+
+<style>
+#modifiers div,
+#modifiers p,
+#modifiers span {
+	padding: 40px;
+	font-size: 40px;
+	font-weight: bold;
+}
+
+#modifiers div {
+	background-color: #ccc;
+}
+
+#modifiers p {
+	background-color: #999;
+}
+
+#modifiers span {
+	background-color: #666;
+	display: block;
+}
+</style>
