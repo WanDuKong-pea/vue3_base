dacf192cfcbc172930994c1ba775f30ea7cd5d19
KimYeJin <sophisticateo_o@naver.com>
2023-12-26 14:10:19 +0900

    디렉티브

---
+++ src/lecture/App_v-memo.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<div>
+			<h1>original</h1>
+			<p>subscribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<hr />
+		<div v-once>
+			<h1>v-once</h1>
+			<p>subscribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<hr />
+		<div v-memo="[views]">
+			<h1>v-memo</h1>
+			<p>subscribers: {{ subscribers }}</p>
+			<p>views: {{ views }}</p>
+			<p>likes: {{ likes }}</p>
+		</div>
+		<button @click="subscribers++">subscribers++</button>
+		<button @click="views++">views++</button>
+		<button @click="likes++">likes++</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+	setup() {
+		const subscribers = ref(4000);
+		const views = ref(400);
+		const likes = ref(200);
+		return {
+			subscribers,
+			views,
+			likes,
+		};
+	},
+};
+</script>
+
+<style></style>
